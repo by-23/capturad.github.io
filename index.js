21d413d41837d552dfb86f63a868adfd12eba2f6
Omnomnom23 <73724646+Omnomnom23@users.noreply.github.com>
2020-10-31 03:28:29 +0300

    index.js восстановил

---
+++ index.js
@@ -0,0 +1,36 @@
+var settings = {
+    cover: false,
+    focus: 'center',
+    autoWidth: true,
+    updateOnMove: true,
+    pagination: false,
+    type: 'loop',
+}
+if ("ontouchstart" in document.documentElement) {
+    settings.isNavigation = false;
+    settings.arrows = true;
+    settings.drag = true;
+}
+else {
+    settings.isNavigation = true;
+    settings.arrows = false;
+    settings.drag = false;
+}
+
+// Create and mount the thumbnails slider.
+var secondarySlider = new Splide('#secondary-slider', settings).mount();
+
+// Create the main slider.
+var primarySlider = new Splide('#primary-slider', {
+    type: 'fade',
+    heightRatio: 0.5,
+    pagination: false,
+    arrows: false,
+    cover: true,
+    autoWidth: true,
+    autoHeight: true,
+});
+
+// Set the thumbnails slider as a sync target and then call mount.
+primarySlider.sync(secondarySlider).mount();
+
