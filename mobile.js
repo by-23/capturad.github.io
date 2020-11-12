7ea115b269918995683ed151d1529577ca4a075b
Omnomnom23 <73724646+Omnomnom23@users.noreply.github.com>
2020-11-12 12:16:24 +0300

    mobile new

--- mobile.js
+++ mobile.js
@@ -1,13 +1,43 @@
-var menuBtn = document.querySelector('.menu-btn');
-var nav = document.querySelector('nav');
-var lineOne = document.querySelector('nav .menu-btn .line--1');
-var lineTwo = document.querySelector('nav .menu-btn .line--2');
-var lineThree = document.querySelector('nav .menu-btn .line--3');
-var link = document.querySelector('nav .nav-links');
-menuBtn.addEventListener('click', () => {
-    nav.classList.toggle('nav-open');
-    lineOne.classList.toggle('line-cross');
-    lineTwo.classList.toggle('line-fade-out');
-    lineThree.classList.toggle('line-cross');
-    link.classList.toggle('fade-in');
-})
+var settings = {
+    cover: false,
+    focus: 'center',
+    autoWidth: true,
+    updateOnMove: true,
+    pagination: false,
+    type: 'loop',
+}
+if ("ontouchstart" in document.documentElement) {
+    settings.isNavigation = false;
+    settings.arrows = true;
+    settings.drag = true;
+}
+else {
+    settings.isNavigation = true;
+    settings.arrows = false;
+    settings.drag = false;
+}
+
+// Create and mount the thumbnails slider.
+var textSlider = new Splide('#text-slider', settings).mount();
+
+// Create the main slider.
+var imageSlider = new Splide('#image-slider', {
+    type: 'fade',
+    heightRatio: 0.5,
+    pagination: false,
+    arrows: false,
+    cover: true,
+    autoWidth: true,
+    autoHeight: true,
+    video: {
+        autoplay: true,
+        mute: true,
+        disableOverlayUI: true,
+        hideControls: true,
+        loop: true,
+    },
+
+});
+
+// Set the thumbnails slider as a sync target and then call mount.
+imageSlider.sync(textSlider).mount(window.splide.Extensions);
