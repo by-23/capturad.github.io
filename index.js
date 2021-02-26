d6e86037b8bc94df34c6f9e1b52210a8580142b7
capturad <bayram.23.070@gmail.com>
2021-02-26 13:35:12 +0300

    Sitemap updated Social network links fixed for mobile devices Neon light effect improved

---
+++ index.js
@@ -0,0 +1,57 @@
+const menuList = document.querySelectorAll('#menu li a')
+var currentIteration = 0
+
+menuList.forEach(element => {
+    element.animate([
+        {
+            filter: 'drop-shadow(0 0 2rem blue)',
+            textShadow: '0 0 1rem blue',
+            boxShadow: '0 0 2rem blue, inset 0 0 2rem blue'
+        },
+        {
+            filter: 'drop-shadow(0 0 2rem green)',
+            textShadow: '0 0 1rem green',
+            boxShadow: '0 0 2rem green, inset 0 0 2rem green'
+        },
+        {
+            filter: 'drop-shadow(0 0 2rem red)',
+            textShadow: '0 0 1rem red',
+            boxShadow: '0 0 2rem red, inset 0 0 2rem red'
+        },
+        {
+            filter: 'drop-shadow(0 0 2rem blue)',
+            textShadow: '0 0 1rem blue',
+            boxShadow: '0 0 2rem blue, inset 0 0 2rem blue'
+        }
+    ],
+        {
+            duration: 10000,
+            iterations: Infinity,
+            iterationStart: currentIteration
+        })
+    currentIteration += 0.25
+})
+const footerList = document.querySelectorAll('footer a')
+
+footerList.forEach(element => {
+    element.animate([
+        {
+            filter: 'drop-shadow(0 0 .4rem blue)',
+        },
+        {
+            filter: 'drop-shadow(0 0 .4rem green)',
+        },
+        {
+            filter: 'drop-shadow(0 0 .4rem red)',
+        },
+        {
+            filter: 'drop-shadow(0 0 .4rem blue)',
+        }
+    ],
+        {
+            duration: 10000,
+            iterations: Infinity,
+            iterationStart: currentIteration
+        })
+    currentIteration += 0.25
+})
