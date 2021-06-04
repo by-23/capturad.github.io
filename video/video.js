b50f9463f50ec0a8fed5a8c939b90b9e095eb4fa
Ekrem Bayram <37544619+ekrem007@users.noreply.github.com>
2021-06-04 20:38:24 +0300

    Template videos replaced to real ones
    Video page visual and other bugs fixed

--- video/video.js
+++ video/video.js
@@ -1,11 +1,9 @@
-// I will be creating a different pen with touch support but right // now I have no time for it due to school
-
 const slider = document.querySelector(".items");
 const slides = document.querySelectorAll(".item");
 const button = document.querySelectorAll(".button");
 
 let current = 0;
-let prev = 4;
+let prev = slides.length - 1;
 let next = 1;
 
 for (let i = 0; i < button.length; i++) {
@@ -14,7 +12,7 @@ for (let i = 0; i < button.length; i++) {
 
 const gotoPrev = () => current > 0 ? gotoNum(current - 1) : gotoNum(slides.length - 1);
 
-const gotoNext = () => current < 4 ? gotoNum(current + 1) : gotoNum(0);
+const gotoNext = () => current < slides.length - 1 ? gotoNum(current + 1) : gotoNum(0);
 
 const gotoNum = number => {
     current = number;
@@ -28,12 +26,12 @@ const gotoNum = number => {
         slides[i].classList.remove("next");
     }
 
-    if (next == 5) {
+    if (next == slides.length) {
         next = 0;
     }
 
     if (prev == -1) {
-        prev = 4;
+        prev = slides.length - 1;
     }
 
     slides[current].classList.add("active");
@@ -42,39 +40,19 @@ const gotoNum = number => {
     slides[next].classList.add("next");
 }
 
-for (const video of document.getElementsByTagName('video')) {
-    var settings = {
-        muted: true,
-        controls: ['play', 'fullscreen']
-    }
-    if (video.parentElement.classList.contains("active")) {
-        settings.autoplay = true;
-    }
-    var plyr = video.parentElement.plyr = new Plyr(video, settings);
-    plyr.on("enterfullscreen", () => {
-        // plyr.muted = false;
-        plyr.volume = 1;
-    })
+const settings = {
+    muted: true,
+    loop: { active: true },
+    storage: { enabled: false },
+    controls: ['play', 'fullscreen']
 }
 
-// var settings = {
-//     video: {
-//         autoplay: true,
-//         mute: true,
-//         disableOverlayUI: true,
-//         hideControls: true,
-//         loop: true
-//     },
-//     trimSpace: true,
-//     height: "75vh",
-//     perPage: 1.5,
-//     updateOnMove: true,
-//     pagination: false,
-//     focus: "center",
-//     type: 'loop',
-//     direction: 'ttb',
-//     cover: true,
-//     arrows: false
-// }
-
-// var videoSlider = new Splide('#video-slider', settings).mount(window.splide.Extensions);
+for (const video of document.getElementsByTagName('video')) {
+    var plyr = video.parentElement.plyr = new Plyr(video, settings);
+    plyr.on("enterfullscreen", (event) => {
+        event.detail.plyr.muted = false;
+    })
+    plyr.on("exitfullscreen", (event) => {
+        event.detail.plyr.muted = true;
+    })
+}
